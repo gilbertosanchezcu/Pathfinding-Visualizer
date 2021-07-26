03dc4dc78b319fec5b7bfa878adcd55b8836791a
Gilberto Sanchez <gilbertosanchez@Gilbertos-MacBook-Air.local>
2021-07-26 16:17:07 -0700

    Modified the 'Notepad' section css. Notepad text stays in place while text is scrollable

--- src/Notepad/MessageBoard.js
+++ src/Notepad/MessageBoard.js
@@ -9,11 +9,14 @@ const MessageBoard = props => {
         <div className='messageboard-container'>
             
             <div className='message-board'>
-                <h3>Notepad</h3>
-                {props.notes.map((note, index) => 
-                    <SingleComment username={note.username} note={note.message} key ={index}/>
-                )}
-
+                <div className = 'message-board-heading'>
+                    <h3>Notepad</h3>
+                </div>
+                <div className = 'message-board-post'>
+                    {props.notes.map((note, index) => 
+                        <SingleComment username={note.username} note={note.message} key ={index}/>
+                    )}
+                </div>
             </div>
 
         </div>
